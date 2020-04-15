8e184a68a81cc4bec73a753cb95d7d773271a292
Lion-Ga92 <Luis@tamalesgarcia.com>
2020-04-14 23:39:57 -0700

    i added a function with a parameter as apples

--- print-state.py
+++ print-state.py
@@ -6,4 +6,9 @@ print(string1 + string2 + str(age))
 print("How old will you be in a decade?")
 age += added_years
 print (str(age))
+# let us add a simple output  print statement with a special_item parameter and call the function with special_item= apples
+def create_string(special_item):
+    print("My favorite food is " + special_item + " and yours?")
+create_string("Apples")
+
 

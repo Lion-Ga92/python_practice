dde8c3d2c2252ea2e54b24ceddca8762ec2e3afc
Lion-Ga92 <Luis@tamalesgarcia.com>
2020-04-14 23:56:56 -0700

    i added a 3X dual if then state

--- print-state.py
+++ print-state.py
@@ -18,5 +18,20 @@ def simple_if_then(num):
         return False
 result = simple_if_then(7)
 print(result)
+# ok now let us try to add to if statements, starting with an if and moving to an elif than giving an else statment to close it if the conditions are not true
 
+def dual_if_then(num1, num2):
+    if num1 == num2:
+        return "numbers are equal"
+    elif num1 > num2:
+        return "number one is greater than number two"
+    else:
+        return "number one is smaller than number two"
+
+result = dual_if_then(4 , 4)
+result2 = dual_if_then(5, 4)
+result3 = dual_if_then(3, 4)
+print(result)
+print(result2)
+print(result3)
 
